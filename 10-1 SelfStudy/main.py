317fe58e885881edcf1270e9bc8b87eac7b8a673
jeseok2658 <andy-yu@naver.com>
2025-05-30 16:31:00 +0900

    Update main.py

--- 10-1 SelfStudy/main.py
+++ 10-1 SelfStudy/main.py
@@ -1,11 +1,11 @@
 from tkinter import *
 window = Tk()
 
-photo1 = PhotoImage(file = "C:SelfStudy10-1\\cat1.gif")
+photo1 = PhotoImage(file = "고양이1.gif")
 label1 = Label(window, image = photo1)
 label1.pack(side=LEFT)
 
-photo2 = PhotoImage(file = "C:SelfStudy10-1\\cat2.gif")
+photo2 = PhotoImage(file = "고양이2.gif")
 label2 = Label(window, image = photo2)
 label2.pack(side=LEFT)
 
