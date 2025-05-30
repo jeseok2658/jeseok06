4a928d8d596d65bfe70ce0ddb09565b1c334df22
jeseok2658 <andy-yu@naver.com>
2025-05-30 16:25:17 +0900

    Update main.py

--- 10-4 SelfStudy/main.py
+++ 10-4 SelfStudy/main.py
@@ -1,1 +1,21 @@
+from tkinter import *
+from tkinter import messagebox
 
+def keyEvent(event):
+    messagebox.showinfo("키보드 이벤트","눌린 키 :"+chr(event.keycode))
+
+def keyEvent(event):
+    key = event.keysym
+    is_shift = (event.state % 2) == 1
+
+
+    if key in ["Up", "Down", "Left", "Right"]:
+        if is_shift:
+            key = "Shift+" + key
+        messagebox.showinfo("키보드 이벤트", "눌린 키 : " + key)
+        
+window=Tk()
+
+window.bind("<Key>",keyEvent)
+
+window.mainloop()
