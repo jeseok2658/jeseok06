56ddc026e02fa413af9248b242f8ce42d986c92e
jeseok2658 <andy-yu@naver.com>
2025-06-06 20:04:49 +0900

    Update main.py

--- 개인작업물/이하창놈/main.py
+++ 개인작업물/이하창놈/main.py
@@ -3,7 +3,7 @@ from tkinter import *
 window.Tk()
 
 window.Title("이하창놈")
-window.geometry("1000x700")
+window.geometry("850x600")
 
 label1=Label1(window,text="나는 첼시에서 행복하지 않다.",font=("궁서체",50),fg="brown",bg="blue")
 photo=PhotoImage(file="uGR-naglw3v_dJadrPT-tjZNWnJk291j9YM8HRY9Gy2-KOu4d0lN6D-8eNd59Q4UvTXBTWERaWj0PlBab20cg1r11OXt7QPs89X6.gif")
